4ab9f83df9ad86bf5eb3dad0333f9daf8a8b4a71
Rosy <rosykidd15@googlemail.com>
2026-05-19 02:36:59 +0600

    Add note about 2013 support in btools.lua
    Added a comment indicating that 2013 is not supported and suggesting Btools2013 for support.

--- all eras/btools.lua
+++ all eras/btools.lua
@@ -1,3 +1,4 @@
+-- 2013 aint supported, for 2013 support, use Btools2013 
 p = game.Players.LocalPlayer
 Instance.new("HopperBin", p.Backpack).BinType = 1
 Instance.new("HopperBin", p.Backpack).BinType = 2
