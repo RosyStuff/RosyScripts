a92f58a2f1056536fc6da0b8ee3583ddc311ee16
Rosy <rosykidd15@googlemail.com>
2026-05-19 02:37:29 +0600

    Create Btools2013.lua

---
+++ all eras/Btools2013.lua
@@ -0,0 +1,3 @@
+Instance.new("HopperBin",game.Players.LocalPlayer.Backpack).BinType = Enum.BinType.Grab
+Instance.new("HopperBin",game.Players.LocalPlayer.Backpack).BinType = Enum.BinType.Clone
+Instance.new("HopperBin",game.Players.LocalPlayer.Backpack).BinType = Enum.BinType.Hammer
