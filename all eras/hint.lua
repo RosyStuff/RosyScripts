f99f05c9d2f665773a109d0679225cce282bbea8
Rosy <rosykidd15@googlemail.com>
2026-07-13 22:27:03 +0600

    fix some fucking shit

--- all eras/hint.lua
+++ all eras/hint.lua
@@ -2,6 +2,6 @@ text = "wsp guys" -- change to your text you like
 duration = 3 -- change to your duration you like
 -- do not edit anything below
 msg = Instance.new('Hint', workspace)
-msg = text
+msg.Text = text
 wait(duration)
 msg:Destroy(-1) -- dont mind the -1 lol
